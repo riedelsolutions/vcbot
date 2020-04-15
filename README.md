a20a38be19a9d0132c8986726cc8e675eaaa89ac
Cam <riedelgcamila@gmail.com>
2020-04-15 19:04:19 -0300

    initial

--- README.md
+++
@@ -1,2 +0,0 @@
-# vcbot
-vcBot is a Discord bot that creates the @vc role and assigns it to the people joining a voice channnel, then unassigns it when they leave. 
--- config.py
+++
@@ -1,5 +0,0 @@
-# The channel to send the log messages to. I won't need ths I need the role tocreate on init/join and assign instead
-CHANNEL_NAME = "general"
-
-# The bot token
-BOT_TOKEN = "Njk5NzI2MjY1ODQ1ODA5MjIz.XpYmgg.SY6nnlnnQ4Z4cq5YTyO5DgTlitY"
